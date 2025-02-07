9b6533cb0e60a662abdce628175db58e38d53708
joSincal <106826998+joSincal@users.noreply.github.com>
2025-02-07 17:21:51 -0600

    end section 14

---
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router";
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+    return alter_ego === characters ? <></> : <p>{characters}</p>;
+};
+
+export const HeroCard = ({
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+}) => {
+    const heroImageUrl = `/heroes/${id}.jpg`;
+
+    return (
+        <div className="col animate__animated animate__fadeIn">
+            <div className="card">
+                <div className="row no-gutters">
+                    <div className="col-4">
+                        <img
+                            className="card-img"
+                            src={heroImageUrl}
+                            alt={superhero}
+                        />
+                    </div>
+
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{superhero}</h5>
+                            <p className="card-text">{alter_ego}</p>
+
+                            <CharactersByHero
+                                alter_ego={alter_ego}
+                                characters={characters}
+                            />
+
+                            <p className="card-text">
+                                <small className="text-muted">
+                                    {first_appearance}
+                                </small>
+                            </p>
+
+                            <Link to={`/hero/${id}`}>Mas...</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+HeroCard.propTypes = {
+    id: PropTypes.string.isRequired,
+    superhero: PropTypes.string.isRequired,
+    publisher: PropTypes.string.isRequired,
+    alter_ego: PropTypes.string.isRequired,
+    first_appearance: PropTypes.string.isRequired,
+    characters: PropTypes.string.isRequired,
+};
